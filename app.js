5c60415d62c8700121764b61f01afccf360e7a44
Dan_Kempert <dwkempert@gmail.com>
2021-11-24 13:09:21 -0500

    preliminary grid layout

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+"use strict";
+
+// let player1 = prompt("Enter the name of Player 1.", "John Doe")
+// let player2 = prompt("Enter the name of Player 2.", "Jane Doe")
+
 console.log("connected")
+// console.log(player1)
+// console.log(player2)
