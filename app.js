20172fcf36c4667902468efef6bc079d4c40b5cc
Dan_Kempert <dwkempert@gmail.com>
2021-11-24 13:57:55 -0500

    preliminary js ADDED 3X3 GRID minor css

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-"use strict";
+const gameState = {
+    players: ["x", "o"],
+    board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ]
+}
 
-// let player1 = prompt("Enter the name of Player 1.", "John Doe")
-// let player2 = prompt("Enter the name of Player 2.", "Jane Doe")
+function resetGame () {
+    gameState.players = ["x", "o"];
+    gameState.board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ]
+}
 
-console.log("connected")
-// console.log(player1)
-// console.log(player2)
+const grid = document.getElementById("gameGrid")
+
+function makeGrid () {
+    for (let i = 0; i < 3; i++) {
+        const row = document.createElement("tr")
+        for (let j = 0; j < 3; j++) {
+            const column = document.createElement("td")
+            row.appendChild(column)
+            console.log("column made")
+        }
+        grid.appendChild(row)
+        console.log("row made")
+    }
+}
+
+makeGrid()
