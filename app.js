45f3d876588d9f6ca0d87a90c30605ed402408b1
Dan_Kempert <dwkempert@gmail.com>
2021-11-28 10:09:14 -0500

    started over - simple HTML - simple CSS - working JS preliminary grid create and preliminary player marking

--- app.js
+++ app.js
@@ -1,76 +1,7 @@
-// INITIAL ATTEMPT AT SCRIPT
-// const gameState = {
-//     players: ["x", "o"],
-//     board: [
-//         [null, null, null],
-//         [null, null, null],
-//         [null, null, null]
-//     ]
-// }
-
-// // Reset a 3x3 game grid and player names
-// function resetGame () {
-//     gameState.players = ["x", "o"];
-//     gameState.board = [
-//         [null, null, null],
-//         [null, null, null],
-//         [null, null, null]
-//     ]
-// }
-
-// const grid = document.getElementsByTagName("table")[0]
-
-// /*  Check if a game grid is present on the page.
-//     If there is no grid, create a 3x3 grid.
-//     If there is a grid, do nothing. */
-// function makeGrid () {
-//     let tableCheck = document.getElementsByTagName("tr")[0]
-//     console.log(tableCheck)
-//     if (tableCheck) {
-//         console.log("I'm in the if!")
-//     }
-//     else {
-//         for (let i = 0; i < 3; i++) {
-//             const row = document.createElement("tr")
-//             for (let j = 0; j < 3; j++) {
-//                 const column = document.createElement("td")
-//                 row.appendChild(column)
-//                 console.log("column made")
-//             }
-//             grid.appendChild(row)
-//             console.log("row made")
-//         }
-//     }
-// }
-
-// // Executes the makeGrid function on the Create Game Grid button click
-// const createGrid = document.getElementById("createGrid")
-// createGrid.addEventListener("click", makeGrid)
-
-
-// const currentMark = "X"
-// function createMark (event) {
-//     console.log("createMark called")
-//     const cell = event.target
-//     if (event.target !== grid) {
-//         console.log("target is not the table")
-//         if (!cell.innerText) {
-//             console.log("trying to place mark in cell")
-//             cell.innerText = currentMark
-//         }
-//         else {
-//             console.log("there is already a mark there!")
-//         }
-//     }
-//     else {
-//         console.log("the target is the grid!")
-//     }
-// }
-
-// grid.addEventListener("click", createMark)
-
 const gameState = {
-    players: ["x", "o"],
+    playerMarks: ["X", "O"],
+    playerNames: ["Player 1", "Player 2"],
+    playerTurn: "X",
     board: [
         [null, null, null],
         [null, null, null],
@@ -78,3 +9,34 @@ const gameState = {
     ]
 }
 
+// create a 3x3 game grid in table id gameGrid
+const gameGrid = document.getElementsByTagName("table")[0]
+function createGameGrid () {
+    for (let i = 0; i < 3; i++) {
+        const row = document.createElement("tr")
+        for (let j = 0; j < 3; j++) {
+            const cell = document.createElement("td")
+            row.appendChild(cell)
+        }
+        gameGrid.appendChild(row)
+    }
+}
+
+createGameGrid()
+
+// creates a mark on the game grid for the player whose turn it is
+function createMark (event) {
+    const targetCell = event.target
+    if (event.target !== gameGrid) {
+        if (gameState.playerTurn == "X") {
+            gameState.playerTurn = "O"
+            targetCell.innerText = "X"
+        }
+        else {
+            gameState.playerTurn = "X"
+            targetCell.innerText = "O"
+        }
+    }
+}
+
+gameGrid.addEventListener("click", createMark)
