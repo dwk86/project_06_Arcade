544ee6b7cdf36652e1288770f86063c966b0c44f
Dan_Kempert <dwkempert@gmail.com>
2021-11-29 14:10:06 -0500

    updated button id for play again - added play again button click logic - added logic to determine if game has ended in a tie

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+// object for holding the state of the game at any time
 const gameState = {
     playerMarks: ["X", "O"],
     playerNames: ["", ""],
     playerNamesLoaded: false,
     playerWins: [0, 0],
     playerTurn: "X",
-    gameOver: false,
+    gameWon: false,
+    gameTied: false,
     board: [
         [null, null, null],
         [null, null, null],
@@ -12,6 +14,8 @@ const gameState = {
     ]
 }
 
+/* bunch of constants for interacting with DOM
+   probably could be done a bit cleaner */
 const gameGrid = document.getElementById("gameGrid")
 const resetButton = document.getElementById("resetButton")
 const updatePlayersButton = document.getElementById("updatePlayersButton")
@@ -22,6 +26,7 @@ const playerOneNameDisplay = document.getElementById("playerOneNameDisplay")
 const playerTwoNameDisplay = document.getElementById("playerTwoNameDisplay")
 const playerOneWinsDisplay = document.getElementById("playerOneWinsDisplay")
 const playerTwoWinsDisplay = document.getElementById("playerTwoWinsDisplay")
+const playAgainButton = document.getElementById("playAgainButton")
 
 // create a 3x3 game grid in table id gameGrid
 function createGameGrid() {
@@ -38,29 +43,41 @@ function createGameGrid() {
 // creates a mark on the game grid for the player whose turn it is
 function createMark(event) {
     const targetCell = event.target
+    // if the target of the click event is not a cell, skip the game logic
     if (event.target !== gameGrid) {
-        if (!gameState.gameOver && gameState.playerNamesLoaded) {
+        // if the game isn't won or tied and the player names have been loaded, go through the game logic
+        if (!gameState.gameWon && !gameState.gameTied && gameState.playerNamesLoaded) {
+            // if the target cell of the click is empty, place a mark
             if (!targetCell.innerText) {
+                // mark the cell according to the player whose turn it is
                 if (gameState.playerTurn == "X") {
                     targetCell.innerText = gameState.playerMarks[0]
                 } else {
                     targetCell.innerText = gameState.playerMarks[1]
                 }
                 updateBoardState()
+                // switch whose turn it is
                 if (gameState.playerTurn == "X") {
                     gameState.playerTurn = "O"
                 } else {
                     gameState.playerTurn = "X"
                 }
             }
-            gameState.gameOver = checkBoardRows()
-            if (!gameState.gameOver) {
-                gameState.gameOver = checkBoardColumns()
+            // check to see if a win condition is in any row
+            gameState.gameWon = checkBoardRows()
+            // if the game hasn't been won by rows, check to see if it is by columns
+            if (!gameState.gameWon) {
+                gameState.gameWon = checkBoardColumns()
             }
-            if (!gameState.gameOver) {
-                gameState.gameOver = checkBoardDiagonals()
+            /* if the game isn't won by rows or columns, see if it is by diagonals
+               !!!!!CURRENTLY ONLY WORKS FOR 3X3 GRID!!!!! */
+            if (!gameState.gameWon) {
+                gameState.gameWon = checkBoardDiagonals()
             }
-            if (gameState.gameOver) {
+            /* if the game is won, increment that player's win count
+               I KNOW THE LOGIC HERE IS WONKY. I HAVE THE PLAYER'S TURN SWITCHING TOO EARLY.
+               I INTEND TO FIX THIS IF I CAN COME BACK TO IT IN TIME. IF NOT, THIS MAKES IT WORK FOR NOW. */
+            if (gameState.gameWon) {
                 if (gameState.playerTurn == "O") {
                     gameState.playerWins[0]++
                     playerOneWinsDisplay.innerText = `Wins: ${gameState.playerWins[0]}`
@@ -69,6 +86,13 @@ function createMark(event) {
                     gameState.playerWins[1]++
                     playerTwoWinsDisplay.innerText = `Wins: ${gameState.playerWins[1]}`
                 }
+            }
+            /* checks to see if the game isn't won and the board is filled.
+               if it hasn't been won and the board is filled, the game is tied
+               since there are no cells left to play in. */
+            if (!gameState.gameWon) {
+                gameState.gameTied = checkBoardIsFilled()
+                console.log(gameState.gameTied)
             }
         }
     }
@@ -119,6 +143,16 @@ function checkBoardColumns() {
     return false
 }
 
+// checks if grid is completely filled
+function checkBoardIsFilled () {
+    const td = document.getElementsByTagName("td")
+    for  (let i = 0; i < td.length; i++) {
+        if (!td[i].innerText) {
+            return false
+        }
+    }
+    return true
+}
 
 // resets game completely
 function resetGame() {
@@ -128,7 +162,8 @@ function resetGame() {
     gameState.playerNamesLoaded = false
     gameState.playerWins = [0, 0]
     gameState.playerTurn = "X"
-    gameState.gameOver = false
+    gameState.gameWon = false
+    gameState.gameTied = false
     gameState.board = [
         [null, null, null],
         [null, null, null],
@@ -158,10 +193,39 @@ function loadPlayerNames() {
     }
 }
 
+// creates 3x3 grid. intended to be replaced with ability to create custom grids
 createGameGrid()
+/* logic for placing mark on the game board. also houses logic for win condition
+   and switching the player whose turn it is */
 gameGrid.addEventListener("click", createMark)
+// completely resets the game state to as if the page was reloaded for new players
 resetButton.addEventListener("click", resetGame)
+// places player names into the game board from the menu
 updatePlayersButton.addEventListener("click", loadPlayerNames)
+// just places Computer as the name for player 2
 isPlayerTwoComputerButton.addEventListener("click", function () {
     playerTwoNameInput.value = "Computer"
+})
+/* resets the game for playing again. is different than the reset game button
+   because it doesn't reset player names, wins, or symbols */
+playAgainButton.addEventListener("click", function () {
+    const td = document.getElementsByTagName("td")
+    if (gameState.playerTurn = "X") {
+        gameState.playerTurn = "O"
+    }
+    else {
+        gameState.playerTurn = "X"
+    }
+    gameState.gameWon = false
+    gameState.gameTied = false
+    gameState.board = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ]
+    for (let i = 0; i < td.length; i++) {
+        td[i].innerText = ""
+    }
+    playerOneNameInput.value = ""
+    playerTwoNameInput.value = ""
 })
